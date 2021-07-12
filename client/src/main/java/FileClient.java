e968861c852527d908f345389bdbf20e33c69a7c
Boyu Fan <51293961+boyufan@users.noreply.github.com>
2021-07-12 16:57:02 +0800

    Add files via upload

---
+++ client/src/main/java/FileClient.java
@@ -0,0 +1,262 @@
+import java.io.*;
+import java.net.Socket;
+import java.nio.file.Files;
+
+public class FileClient {
+
+
+    // Connection details
+    public int id;
+
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+
+    public static String uploadDir="res/clientModel/";
+    public static String downloadDir="res/serverModel/";
+
+    private FileClient(Socket socket, int id, DataInputStream input, DataOutputStream output) {
+        this.socket = socket;
+        this.id = id;
+        this.in = input;
+        this.out = output;
+        System.out.println(id);
+    }
+
+    // Factory method to create a client instance
+    public static FileClient connect(String ip, int port, int timeout) {
+        try {
+            System.out.println("Connecting to server...");
+            Socket socket = new Socket(ip, port);
+            System.out.println("The new socket object is" + socket);
+            socket.setSoTimeout(timeout);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+            System.out.println("Connected");
+            int id = in.readInt();
+            System.out.println("The client id is: " + id);
+            return new FileClient(socket, id, in, out);
+        } catch (IOException e) {
+            // Handle errors
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    // Attempts to quit gracefully using operations
+    public void quit() {
+        try {
+            out.writeUTF("QUIT");
+            out.close();
+            in.close();
+            socket.close();
+        } catch (IOException e) {
+            System.out.println("Error quitting gracefully (" + e.getMessage() + ")");
+            System.out.println("Force closing");
+
+            // Force close
+            try {
+                out.close();
+            } catch (IOException f) { /* Do nothing */ }
+            try {
+                in.close();
+            } catch (IOException f) { /* Do nothing */ }
+            try {
+                socket.close();
+            } catch (IOException f) { /* Do nothing */ }
+
+        }
+        System.out.println("Session closed");
+    }
+
+    private byte[] downloadFromServer(String filename) throws IOException {
+        // Send operation and filename
+        System.out.println("Sending DWLD operation to server...");
+        out.writeUTF("DWLD");
+        out.writeShort(filename.length());
+        out.writeChars(filename);
+
+        // Read server response, handle weird values (out of spec)
+        int fileSize = in.readInt();
+        if (fileSize == -1) {
+            System.out.println("File does not exist on server");
+            return null;
+        } else if (fileSize < 0) {
+            System.out.println("Negative integer returned for filesize that was not -1. Download cancelled");
+            return null;
+        }
+
+        // Confirm readiness to download
+        out.writeBoolean(true);
+        System.out.println("Downloading from server...");
+
+        // Declare our array of bytes
+        byte[] bytes = new byte[fileSize];
+        int totBytesRead = 0;
+
+        // Read as many bytes as possible until buffer is full
+        while (totBytesRead < fileSize) {
+            int bytesRead = in.read(bytes, totBytesRead, fileSize - totBytesRead);
+            totBytesRead += bytesRead;
+        }
+
+        return bytes;
+    }
+
+    boolean download(String filename) throws IOException {
+        // Start timer
+        long startTime = System.currentTimeMillis();
+
+        // Download bytes from server
+        byte[] bytes;
+        try {
+            bytes = downloadFromServer(filename);
+        } catch (IOException e) {
+            // Handle errors, errors here should cause a disconnect
+            e.printStackTrace();
+            return false;
+        }
+
+        if (bytes != null) {
+            // Write file out
+            // download the model file from server and save in the onDeviceModel folder
+            String localpath = downloadDir + filename;
+            File outFile = new File(localpath);
+            FileOutputStream stream = new FileOutputStream(outFile);
+            stream.write(bytes);
+
+            // Gather statistics
+            long endTime = System.currentTimeMillis();
+            double timeTaken = (endTime - startTime);
+            timeTaken /= 1000;
+            System.out.println(String.format("%,d bytes transferred in %,.2fs", bytes.length, timeTaken));
+        }
+        return true;
+    }
+
+    // Returns false if there is a SERVER error
+    // Client errors (eg. IOException on file read, will still return true)
+    public boolean upload(File file, String filename) {
+        // Read the file as bytes from disk first
+        System.out.println("Reading file from disk");
+        byte[] bytes;
+        try {
+            bytes = Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            // Handle errors. Errors reading file are not fatal to the server-client connection
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return true;
+        }
+
+        // Send the file to the server
+        try {
+            uploadFile(filename, bytes);
+        } catch (IOException e) {
+            // Handle errors
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    // The code that performs the upload (wrapped in upload to handle errors)
+    private void uploadFile(String filename, byte[] bytes) throws IOException {
+        // Send operation, filename, and length of file
+        System.out.println("Sending UPLD operation to server and waiting for response...");
+        out.writeUTF("UPLD");
+        out.writeShort(filename.length());
+        out.writeChars(filename);
+        out.writeInt(bytes.length);
+
+        // Get server confirmation
+        if (!in.readBoolean()) {
+            String reason = in.readUTF();
+            System.out.println("Server rejected request");
+            System.out.println("Reason: " + reason);
+            return;
+        }
+
+        // Send file
+        System.out.println("Sending data to server...");
+        out.write(bytes);
+        System.out.println(in.readUTF());
+    }
+
+    /*
+    private void uploadParameterTable() throws IOException {
+        System.out.println("Send UPLDPT operation to server and waiting for response...");
+        out.writeUTF("UPLDPT");
+        out.write
+    }
+
+     */
+
+
+    public static void main(String args[]) throws IOException, InterruptedException {
+        String DEFAULT_IP = "30.20.0.8";
+        int DEFAULT_PORT = 8000;
+        int DEFAULT_TIMEOUT = 5000;
+
+        // This client is just used for download the model from server
+
+        FileClient c = connect(DEFAULT_IP, DEFAULT_PORT, DEFAULT_TIMEOUT);
+         //download latest model from server
+        c.download("server_model.zip");
+//
+//        //local update
+//        localUpdate localModel = new localUpdate();
+//        localModel.id = c.id + "";
+//        localModel.clientUpdate();
+//
+//        //upload local model to server
+//        c.upload(new File(uploadDir + c.id + ".zip"), c.id + ".zip");
+//
+//        //disconnect
+//        c.quit();
+        MultiClients multiClients = new MultiClients();
+        int numOfClients = 160;
+        for (int i = 0; i < numOfClients; i++) {
+            Thread thread = new Thread(multiClients);
+            thread.start();
+        }
+
+    }
+
+}
+
+class MultiClients implements Runnable {
+
+    @Override
+    public void run() {
+        String DEFAULT_IP = "30.20.0.8";
+//        String DEFAULT_IP = "192.168.0.101";
+        int DEFAULT_PORT = 8000;
+
+        int DEFAULT_TIMEOUT = 5000;
+
+        FileClient c = FileClient.connect(DEFAULT_IP, DEFAULT_PORT, DEFAULT_TIMEOUT);
+        //download latest model from server
+        // Attention!!!
+        // need to download the server_model only once before the multi-thread run,
+        // as reading the .zip file may have conflict when reading and writing run at the same time.
+//        try {
+//            c.download("server_model.zip");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        //local update
+        localUpdate localModel = new localUpdate();
+        localModel.id = c.id + "";
+        localModel.clientUpdate(1);
+
+        //upload local model to server
+        c.upload(new File(FileClient.uploadDir + c.id + ".zip"), c.id + ".zip");
+        //disconnect
+        c.quit();
+    }
+}
---
+++ server/src/main/java/server/FederatedModel.java
@@ -0,0 +1,168 @@
+package server;
+import org.datavec.api.records.reader.RecordReader;
+import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
+import org.datavec.api.split.FileSplit;
+import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.evaluation.classification.Evaluation;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+import org.nd4j.linalg.learning.config.Nesterovs;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class FederatedModel {
+
+    public static int numInputs = 45;
+    public static int numOutputs = 10;
+    public static int batchSize = 50;
+
+    public static MultiLayerNetwork model = null;
+
+    private static final String onDeviceModelPath = "res/clientModel";
+    private static final String serverModel = "res/serverModel/server_model.zip";
+    private static final String updatedModel = "res/serverModel/server_model.zip";
+
+
+    // average weights over mobile devices' models
+    public void AverageWeights(int layer, double alpha, int K) throws IOException {
+        System.out.println("The number of client is: " + K);
+
+        //original model
+        Map<String, INDArray> paramTable = model.paramTable();
+        System.out.println(paramTable);
+        INDArray weight = paramTable.get(String.format("%d_W", layer));
+        INDArray bias = paramTable.get(String.format("%d_b", layer));
+        INDArray avgWeights = weight.mul(alpha);
+        System.out.println("the avgWeight is :\n" + avgWeights);
+        INDArray avgBias = bias.mul(alpha);
+        System.out.println("the avgBias is :\n" + avgBias);
+
+        // average weights over mobile devices' models
+        System.out.println("\nAveraging weights...");
+
+        MultiLayerNetwork transferred_model = null;
+        for (int i = 0; i < K; i++) {
+            if(FileServer.cache.containsKey(i)) {
+                paramTable = FileServer.cache.get(i);
+                System.out.println("the get parameter is :\n" + paramTable);
+                weight = paramTable.get(String.format("%d_W", layer));
+                System.out.println("The client weight is: \n" + weight);
+                bias = paramTable.get(String.format("%d_b", layer));
+                System.out.println("The client bias is: \n" + bias);
+                System.out.println("The process run in there");
+                avgWeights = avgWeights.add(weight.mul(1.0 - alpha).div(K));
+                avgBias = avgBias.add(bias.mul(1.0 - alpha).div(K));
+            }
+        }
+
+        model.setParam(String.format("%d_W", layer), avgWeights);
+        model.setParam(String.format("%d_b", layer), avgBias);
+
+        System.out.println("\nWriting server model...");
+        ModelSerializer.writeModel(model, serverModel, false);
+
+        evaluateModel();
+
+        FileServer.cache.clear();
+    }
+
+    public static void delete(List<File> files) {
+        System.out.println("Deleting files...");
+        int len = files.size();
+        for (int i = 0; i < len; i++) {
+            files.get(i).delete();
+        }
+        System.out.println("Files deleted");
+    }
+
+    public static void evaluateModel() {
+
+        final String filenameTest = "res/dataset/test.csv";
+        //Load the test/evaluation data:
+        RecordReader rrTest = new CSVRecordReader();
+        try {
+            rrTest.initialize(new FileSplit(new File(filenameTest)));
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest, batchSize, 0, 10);
+        System.out.println("\nEvaluate model....");
+        Evaluation eval = new Evaluation(numOutputs);
+        while (testIter.hasNext()) {
+            DataSet t = testIter.next();
+            INDArray features = t.getFeatures();
+            INDArray labels = t.getLabels();
+            INDArray predicted = model.output(features, false);
+            eval.eval(labels, predicted);
+        }
+        // Print the evaluation statistics
+        System.out.println(eval.stats());
+
+    }
+
+
+    public void initModel() throws IOException {
+
+        System.out.println("initing model...");
+
+        int seed = 100;
+        double learningRate = 0.001;
+        int round = 10;
+        int numHiddenNodes = 1000;
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .weightInit(WeightInit.XAVIER)
+                .updater(new Nesterovs(learningRate, 0.9))
+                .list()
+                .layer(new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .activation(Activation.SOFTMAX)
+                        .nIn(numHiddenNodes).nOut(numOutputs).build())
+                .build();
+
+        model = new MultiLayerNetwork(conf);
+
+        model.init();
+        System.out.println("init model finish!\n");
+
+        ModelSerializer.writeModel(model, serverModel, true);
+        System.out.println("Write model to " + serverModel + " finish\n");
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+//        initModel();
+//
+//        writeModel();
+
+//        for (int t = 0; t < round; t++) {
+//            System.out.println("\n Global Training Round:" + t + "\n");
+//            System.out.println("selecting clients...");
+//            selectClients(199);
+//            System.out.println("AverageWeights...");
+//            AverageWeights(2, 0.5);
+//            evaluateModel();
+//        }
+        System.out.println("Done!");
+    }
+
+}
