c9159d9697c2e4f5b713d1447608566429f3d61f
boyufan <fbygg@126.com>
2021-08-28 01:12:17 +0800

    add Raspberry Pi project

---
+++ src/main/java/org/deeplearning4j/examples/sample/MultiClients.java
@@ -0,0 +1,40 @@
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+class MultiClients implements Runnable {
+
+    @Override
+    public void run() {
+        String DEFAULT_IP = "192.168.0.102";
+//        String DEFAULT_IP = "30.20.0.8";
+        int DEFAULT_PORT = 8000;
+
+        int DEFAULT_TIMEOUT = 5000;
+        int layer = 2;
+
+        FileClient c = FileClient.connect(DEFAULT_IP, DEFAULT_PORT, DEFAULT_TIMEOUT);
+
+        //local update
+        localUpdate localModel = new localUpdate();
+        localModel.id = c.id + "";
+        localModel.clientUpdate();
+
+        Map<String, INDArray> map = new HashMap<>();
+        Map<String, INDArray> paramTable = localUpdate.transferred_model.paramTable();
+        map.put("weight", paramTable.get(String.format("%d_W", layer)));
+        map.put("bias", paramTable.get(String.format("%d_b", layer)));
+        try {
+            c.uploadParamTable(map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //upload local model to server
+//        c.upload(new File(FileClient.uploadDir + c.id + ".zip"), c.id + ".zip");
+        //disconnect
+        c.quit();
+    }
+}
